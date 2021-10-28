ccd761f6b0c51ca515b30d2772c2b3dbc4e59c09
Chopin <hector.n.nieva@gmail.com>
2021-10-28 15:50:31 -0300

    first commit

---
+++ src/Calculadora.java
@@ -0,0 +1,16 @@
+
+public class Calculadora {
+	
+	
+	public static int sumar(int primerNumero, int segundoNumero) {
+		return primerNumero + segundoNumero;
+	}
+	
+	public static int restar(int primerNumero, int segundoNumero) {
+		return primerNumero - segundoNumero;
+	}
+	
+	public static int multiplicar(int primerNumero, int segundoNumero) {
+		return primerNumero * segundoNumero;
+	}
+}
---
+++ src/Principal.java
@@ -0,0 +1,12 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Calculadora.sumar(20, 10));
+		System.out.println(Calculadora.restar(40, 7));
+		System.out.println(Calculadora.multiplicar(3, 3));
+
+	}
+
+}
